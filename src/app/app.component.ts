d3cb42383bb063c0de60affd5aa789518ea7ac61
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-02-12 18:34:54 -0300

    feat: cria componente `Apresentacao`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BarraLateralComponent } from "./componentes/barra-lateral/barra-lateral.component";
+import { ApresentacaoComponent } from "./componentes/apresentacao/apresentacao.component";
 
 @Component({
   selector: 'app-root',
-  imports: [BarraLateralComponent],
+  imports: [BarraLateralComponent, ApresentacaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

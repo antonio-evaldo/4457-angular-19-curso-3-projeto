c13b822d5292f2a37f4329fb0d8b37ff9da159ee
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-02-12 18:29:41 -0300

    feat: cria componente `BarraLateral`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BarraLateralComponent } from "./componentes/barra-lateral/barra-lateral.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [BarraLateralComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

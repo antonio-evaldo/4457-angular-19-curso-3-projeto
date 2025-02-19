a58d0eeaf0b72110f0c7530c656401c33b216e91
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-02-19 12:32:15 -0300

    Vídeo 2.1: Criando diretiva personalizada

---
+++ src/app/compartilhados/destaque-valor-numerico.directive.ts
@@ -0,0 +1,12 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appDestaqueValorNumerico]'
+})
+export class DestaqueValorNumericoDirective {
+
+  constructor() {
+    console.log('Diretiva aplicada!');
+  }
+
+}
--- src/app/componentes/area-financeira/transacoes/transacao/transacao.component.ts
+++ src/app/componentes/area-financeira/transacoes/transacao/transacao.component.ts
@@ -1,10 +1,11 @@
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Component, computed, input } from '@angular/core';
 import { TipoTransacao, Transacao } from '../../../../modelos/transacao';
+import { DestaqueValorNumericoDirective } from '../../../../compartilhados/destaque-valor-numerico.directive';
 
 @Component({
   selector: 'app-transacao',
-  imports: [CurrencyPipe, DatePipe],
+  imports: [CurrencyPipe, DatePipe, DestaqueValorNumericoDirective],
   templateUrl: './transacao.component.html',
   styleUrl: './transacao.component.css'
 })

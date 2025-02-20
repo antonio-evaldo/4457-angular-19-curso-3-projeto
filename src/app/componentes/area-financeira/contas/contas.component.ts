1adb9b43d7e4492a6be0e9b6c746e49fb638c776
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-02-20 18:50:58 -0300

    Mão na massa: refatorando o card de contas

--- src/app/componentes/area-financeira/contas/contas.component.ts
+++ src/app/componentes/area-financeira/contas/contas.component.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { BotaoComponent } from "../../botao/botao.component";
 import { ContaComponent } from "./conta/conta.component";
 import { Conta } from '../../../modelos/conta';
+import { CardComponent } from "../card/card.component";
 
 @Component({
   selector: 'app-contas',
-  imports: [BotaoComponent, ContaComponent],
+  imports: [BotaoComponent, ContaComponent, CardComponent],
   templateUrl: './contas.component.html',
   styleUrl: './contas.component.css'
 })

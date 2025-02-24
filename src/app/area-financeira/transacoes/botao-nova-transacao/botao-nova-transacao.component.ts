96d392b330563720d2f69725d45b0736c3b6f8e6
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-02-24 18:17:15 -0300

    Vídeo 5.1: Dinamizando o formulário de nova transação

--- src/app/area-financeira/transacoes/botao-nova-transacao/botao-nova-transacao.component.ts
+++ src/app/area-financeira/transacoes/botao-nova-transacao/botao-nova-transacao.component.ts
@@ -1,17 +1,34 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BotaoComponent } from "../../../compartilhados/botao/botao.component";
 import { ModalComponent } from "../../../compartilhados/modal/modal.component";
+import { FormsModule } from "@angular/forms"
+import { KeyValuePipe } from '@angular/common';
+import { TipoTransacao } from '../../compartilhados/transacao.model';
 
 @Component({
   selector: 'app-botao-nova-transacao',
-  imports: [BotaoComponent, ModalComponent],
+  imports: [BotaoComponent, ModalComponent, FormsModule, KeyValuePipe],
   templateUrl: './botao-nova-transacao.component.html',
   styleUrl: './botao-nova-transacao.component.css'
 })
 export class BotaoNovaTransacaoComponent {
   modalAberto = signal(false);
 
+  novaTransacaoForm = {
+    nome: '',
+    tipo: '',
+    valor: '',
+    data: '',
+    conta: ''
+  };
+
+  tiposTransacao = TipoTransacao;
+
   abrirModal() {
     this.modalAberto.set(true);
+  }
+
+  aoSubmeter() {
+    console.log(this.novaTransacaoForm);
   }
 }
